7f40dc04c54304e6953628566ac351648d933259
Rob Brekelmans <brekelma@usc.edu>
2017-09-19 13:18:07 -0700

    unified

--- corex_continuous.py
+++ corex_continuous.py
@@ -21,7 +21,7 @@ Params = NamedTuple('Params', [
 ])
 
 
-class CorexContinuous:  #(Primitive):
+class CorexContinuous(UnsupervisedLearnerPrimitiveBase):  #(Primitive):
     def __init__(self, n_hidden : int = None, latent_pct : float = None, max_iter : int = 10000, 
             tol : float = 1e-5, anneal : bool = True, discourage_overlap : bool = True, gaussianize : str = 'standard',  
             gpu : bool = False, verbose : bool = False, seed : int = None, **kwargs) -> None:
@@ -29,14 +29,18 @@ class CorexContinuous:  #(Primitive):
         super().__init__()
 
         self.kwargs = kwargs
+        self.max_iter = max_iter
+        self.tol = tol
+        self.anneal = anneal
+        self.discourage_overlap = discourage_overlap
+        self.gaussianize = gaussianize
+        self.gpu = gpu
+        self.verbose = verbose
+        self.seed = seed
 
-        #self.latent_pct = latent_pct if (latent_pct and not n_hidden) else None
-        # if latent_pct is not None and n_hidden is None:
-        #   self.n_hidden = int(len(self.columns)*latent_pct)
-        # else:
-        #   self.n_hidden = 2 if n_hidden is None else n_hidden #DEFAULT = 2 
+        self.is_feature_selection = False
+        self.hyperparameters = {'n_hidden': 2} # NOT TRUE, default = latent pct
         
-        # prev initialization based on n_hidden
         if n_hidden:
             self.n_hidden = n_hidden
             self.model = corex_cont.Corex(n_hidden= self.n_hidden, max_iter = max_iter, tol = tol, 
@@ -48,12 +52,8 @@ class CorexContinuous:  #(Primitive):
             else:
                 self.latent_pct = latent_pct
             self.model = None
-                #self.model = corex_cont.Corex(n_hidden= self.latent_pct, max_iter = max_iter, tol = tol, 
-                #    anneal = anneal, discourage_overlap = discourage_overlap, gaussianize = gaussianize, 
-                #    gpu = gpu, verbose = verbose, seed = seed, **kwargs)
 
-        self.latent_pct = latent_pct if (latent_pct and not n_hidden) else None
- 
+
         # if latent_pct is not None and n_hidden is None:
         # 	self.n_hidden = int(len(self.columns)*latent_pct)
         # else:
@@ -65,8 +65,7 @@ class CorexContinuous:  #(Primitive):
         if not self.training_inputs:
             raise ValueError("Missing training data.")
 
-        X = self.training_inputs
-        self.fit_transform(X)
+        self.fit_transform(self.training_inputs)
         self.fitted = True
         return self
 
@@ -75,7 +74,15 @@ class CorexContinuous:  #(Primitive):
         self.columns = list(X)
         X_ = X[self.columns].values 
     	
+       
+
         if not self.fitted:
+            if self.model is None and self.latent_pct:
+                self.n_hidden = int(self.latent_pct*len(self.columns))
+                self.model = corex_cont.Corex(n_hidden= self.n_hidden, max_iter = self.max_iter, tol = self.tol, 
+                    anneal = self.anneal, discourage_overlap = self.discourage_overlap, gaussianize = self.gaussianize, 
+                    gpu = self.gpu, verbose = self.verbose, seed = self.seed, **self.kwargs)
+
             self.factors = self.model.fit_transform(X_)
         else:
             self.factors = self.model.transform(X_)
@@ -86,11 +93,14 @@ class CorexContinuous:  #(Primitive):
         
         self.columns = list(X)
      	X_ = X[self.columns].values
-        
+
+        if not self.n_hidden:
+            self.n_hidden = int(self.latent_pct*len(self.columns))
+
         if self.model is None and self.latent_pct:
-        	self.model = corex_cont.Corex(n_hidden= int(self.latent_pct*len(self.columns)), max_iter = max_iter, tol = tol, 
-                anneal = anneal, discourage_overlap = discourage_overlap, gaussianize = gaussianize, 
-                gpu = gpu, verbose = verbose, seed = seed, **self.kwargs)
+        	self.model = corex_cont.Corex(n_hidden= self.n_hidden, max_iter = self.max_iter, tol = self.tol, 
+                anneal = self.anneal, discourage_overlap = self.discourage_overlap, gaussianize = self.gaussianize, 
+                gpu = self.gpu, verbose = self.verbose, seed = self.seed, **self.kwargs)
 
     	self.factors = self.model.fit_transform(X_)
         self.fitted = True
