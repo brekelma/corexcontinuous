4c2177354875f1078fa883be857a8a48e07a4f22
Rob Brekelman <brekelma@usc.edu>
2018-03-29 14:23:26 -0700

    update 3.29 api git

--- corex_continuous.py
+++ corex_continuous.py
@@ -4,144 +4,151 @@ import sys
 import os
 import corexcontinuous.linearcorex.linearcorex.linearcorex as corex_cont
 #import LinearCorex.linearcorex as corex_cont
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 from scipy import sparse
 import pandas as pd
 import numpy as np
 
+import d3m.metadata.container as container
+import d3m.metadata.hyperparams as hyperparams
+import d3m.metadata.params as params
+from d3m.metadata.metadata import PrimitiveMetadata
 
-from primitive_interfaces.unsupervised_learning import UnsupervisedLearnerPrimitiveBase
-from typing import NamedTuple, Union, Optional, Sequence
+from d3m.primitive_interfaces.unsupervised_learning import UnsupervisedLearnerPrimitiveBase
+from d3m.primitive_interfaces.base import CallResult
+#from d3m.primitive_interfaces.params import Params
+from d3m.metadata.hyperparams import Uniform, UniformInt, Union, Enumeration
+
+from typing import NamedTuple, Optional, Sequence, Any
+import typing
+
+from .. import config
+
+Input = container.DataFrame
+Output = container.ndarray
+
+class CorexContinuous_Params(params.Params):
+    model:typing.Union[corex_cont.Corex, None]
+    #fitted: bool
+    #training_inputs: Input
+
+    # add support for resuming training / storing model information
 
 
-Input = pd.DataFrame
-Output = np.ndarray
-Params = NamedTuple('Params', [
-    ('latent_factors', np.ndarray), 
-])
+class CorexContinuous_Hyperparams(hyperparams.Hyperparams):
+    n_hidden = Union(OrderedDict([('n_hidden int' , hyperparams.Uniform(lower = 1, upper = 50, default = 2, q = 1, description = 'number of hidden factors learned')),
+        ('n_hidden pct' , hyperparams.Uniform(lower = 0, upper = .50, default = .2, q = .05, description = 'number of hidden factors as percentage of # input columns'))]), 
+        default = 'n_hidden pct')
 
 
-class CorexContinuous(UnsupervisedLearnerPrimitiveBase):  #(Primitive):
+class CorexContinuous(UnsupervisedLearnerPrimitiveBase[Input, Output, CorexContinuous_Params, CorexContinuous_Hyperparams]):  #(Primitive):
     
     """
     Return components/latent factors that explain the most multivariate mutual information in the data under Linear Gaussian model. For comparison, PCA returns components explaining the most variance in the data.  Serves as DSBox 'wrapper' for https://github.com/gregversteeg/linearcorex"
     """
-    __author__ = "Rob Brekelmans <brekelma@usc.edu>, Greg Ver Steeg"
-    __metadata__ = {
-        "team": "ISI DSBox",
-        "common_name": "CorexContinuous",
-        "algorithm_type": ["DimensionalityReduction"],
-        "compute_resources": {
-            "sample_size": [.3424],
-            "sample_unit": ["MB"],
-            "disk_per_node": [],
-            "expected_running_time": [9],
-            "gpus_per_node": [],
-            "cores_per_node": [1],
-            "mem_per_gpu": [],
-            "mem_per_node": [],
-            "num_nodes": [],
-        }
-    }
+    metadata = PrimitiveMetadata({
+      "schema": "v0",
+      "id": "d2d4fefc-0859-3522-91df-7e445f61a69b",
+      "version": config.VERSION,
+      "name": "corexcontinuous.corex_continuous.CorexContinuous",
+      "description": "Return components/latent factors that explain the most multivariate mutual information in the data under Linear Gaussian model. For comparison, PCA returns components explaining the most variance in the data.",
+      "python_path": "d3m.primitives.dsbox.CorexContinuous",
+      "original_python_path": "corexcontinuous.corex_continuous.CorexContinuous",
+      "source": {
+            "name": config.D3M_PERFORMER_TEAM,
+            "contact": config.MAIL_TO,
+            "uris": [ config.REPOSITORY ]
+            },
+      "installation": [
+        config.INSTALLATION 
+       # {
+       #   "type":"PIP",
+       #   "package":"corextext",
+       #  "version":"0.2.10"
+       # }
+        ],
+      "algorithm_types": ["EXPECTATION_MAXIMIZATION_ALGORITHM"],
+      "primitive_family": "FEATURE_CONSTRUCTION",
+      "preconditions": ["NO_MISSING_VALUES", "NO_CATEGORICAL_VALUES"],
+      "hyperparams_to_tune": ["n_hidden"]
+    })
+    #  "effects": [],
 
-    def __init__(self, n_hidden : int = None, latent_pct : float = .2, max_iter : int = 10000, 
-            tol : float = 1e-5, anneal : bool = True, discourage_overlap : bool = True, gaussianize : str = 'standard',  
-            gpu : bool = False, verbose : bool = False, seed : int = None, **kwargs) -> None:
+    #def __init__(self, n_hidden : Any = None, max_iter : int = 10000, 
+    def __init__(self, *, hyperparams : CorexContinuous_Hyperparams, random_seed : int =  0, docker_containers: typing.Dict[str, str] = None) -> None:
+        # Additional Corex Parameters set to defaults:  see github.com/gregversteeg/LinearCorex
         
-        super().__init__()
-
-        self.kwargs = kwargs
-        self.max_iter = max_iter
-        self.tol = tol
-        self.anneal = anneal
-        self.discourage_overlap = discourage_overlap
-        self.gaussianize = gaussianize
-        self.gpu = gpu
-        self.verbose = verbose
-        self.seed = seed
-
-        self.is_feature_selection = False
-        self.hyperparameters = {'n_hidden': None, 'latent_pct': .2} # NOT TRUE, default = latent pct
+        #tol : float = 1e-5, anneal : bool = True, discourage_overlap : bool = True, gaussianize : str = 'standard',  
+        #gpu : bool = False, verbose : bool = False, seed : int = None, **kwargs) -> None:
         
-        if n_hidden:
-            self.n_hidden = n_hidden
-            self.model = corex_cont.Corex(n_hidden= self.n_hidden, max_iter = max_iter, tol = tol, 
-                anneal = anneal, discourage_overlap = discourage_overlap, gaussianize = gaussianize, 
-                gpu = gpu, verbose = verbose, seed = seed, **kwargs)
-        else:
-            self.latent_pct = latent_pct
-            self.model = None
-            self.n_hidden = None
+        super().__init__(hyperparams = hyperparams, random_seed = random_seed, docker_containers = docker_containers)
+        
 
-        # if latent_pct is not None and n_hidden is None:
-        # 	self.n_hidden = int(len(self.columns)*latent_pct)
-        # else:
-        # 	self.n_hidden = 2 if n_hidden is None else n_hidden #DEFAULT = 2 
 
-    def fit(self, timeout: float = None, iterations : int = None) -> None:
+    def fit(self, *, timeout: float = None, iterations : int = None) -> CallResult[None]:
         if self.fitted:
             return
         if not hasattr(self, 'training_inputs'):
             raise ValueError("Missing training data.")
 
-        self.fit_transform(self.training_inputs, timeout, iterations)
+        self._fit_transform(self.training_inputs, timeout, iterations)
         self.fitted = True
-        return self
+        # add support for max_iter / incomplete
+        return CallResult(None, True, self.max_iter)
 
-    def produce(self, inputs : Sequence[Input], timeout : float = None, iterations : int = None) -> Sequence[Output]: 
+    def produce(self, *, inputs : Input, timeout : float = None, iterations : int = None) -> CallResult[Output]: 
 
         self.columns = list(inputs)
         X_ = inputs[self.columns].values 
     	
         if iterations is not None:
             self.max_iter = iterations
+        else:
+            self.max_iter = 10000
 
         if not self.fitted:
-            if self.model is None and self.latent_pct:
-                self.n_hidden = max(1, int(self.latent_pct*len(self.columns)))
-                self.model = corex_cont.Corex(n_hidden= self.n_hidden, max_iter = self.max_iter, tol = self.tol, 
-                    anneal = self.anneal, discourage_overlap = self.discourage_overlap, gaussianize = self.gaussianize, 
-                    gpu = self.gpu, verbose = self.verbose, seed = self.seed, **self.kwargs)
+            raise ValueError('Please fit before calling produce')
 
-            self.latent_factors = self.model.fit_transform(X_)
-        else:
-            self.latent_factors = self.model.transform(X_)
+        self.latent_factors = self.model.transform(X_)
 
-        return self.latent_factors
+        return CallResult(self.latent_factors, True, self.max_iter)
 
-    def fit_transform(self, inputs : Sequence[Input], timeout: float = None, iterations : int = None) -> Sequence[Output]:
+    def _fit_transform(self, inputs : Input, timeout: float = None, iterations : int = None) -> Sequence[Output]:
         
         self.columns = list(inputs)
         X_ = inputs[self.columns].values
 
-        if self.n_hidden is None:
-            self.n_hidden = max(1,int(self.latent_pct*len(self.columns)))
-
         if iterations is not None:
             self.max_iter = iterations
+        else:
+            self.max_iter = 10000
 
+        if isinstance(self.hyperparams['n_hidden'], int):
+            self.n_hidden = self.hyperparams['n_hidden']
+        elif isinstance(self.hyperparams['n_hidden'], float):
+            self.n_hidden = max(1,int(self.hyperparams['n_hidden']*len(self.columns)))
 
-        if self.model is None and self.latent_pct:
-        	self.model = corex_cont.Corex(n_hidden= self.n_hidden, max_iter = self.max_iter, tol = self.tol, 
-                anneal = self.anneal, discourage_overlap = self.discourage_overlap, gaussianize = self.gaussianize, 
-                gpu = self.gpu, verbose = self.verbose, seed = self.seed, **self.kwargs)
+        if not hasattr(self, 'model') or self.model is None:
+            self.model = corex_cont.Corex(n_hidden= self.n_hidden, max_iter = self.max_iter)
 
         self.latent_factors = self.model.fit_transform(X_)
         self.fitted = True
         return self.latent_factors
 
-    def set_training_data(self, inputs : Sequence[Input]) -> None:
+    def set_training_data(self, *, inputs : Input) -> None:
         self.training_inputs = inputs
         self.fitted = False
 
-    def get_params(self) -> Params:
-        return Params(latent_factors = self.latent_factors)
+    def get_params(self) -> CorexContinuous_Params:
+        return CorexContinuous_Params(model = self.model)
 
-    def set_params(self, params: Params) -> None:
-        self.latent_factors = params.latent_factors
+    def set_params(self, *, params: CorexContinuous_Params) -> None:
+        self.model = params['model']
+        #self.fitted = params.fitted
+        #self.training_inputs = params.training_inputs
 
 
-    def annotation(self):
+    def _annotation(self):
         if self._annotation is not None:
             return self._annotation
         self._annotation = Primitive()
@@ -152,6 +159,6 @@ class CorexContinuous(UnsupervisedLearnerPrimitiveBase):  #(Primitive):
         self._annotation.tags = ['feature_extraction', 'continuous']
         return self._annotation
 
-    def get_feature_names(self):
-    	return ['CorexContinuous_'+ str(i) for i in range(self.n_hidden)]
+    def _get_feature_names(self):
+    	return ['CorexContinuous_'+ str(i) for i in range(self.hyperparams['n_hidden'])]
 
